f5adb50296e89b24b0dea1354894d52df29aca4b
Nitin Rana <nitinranagraphian@gmail.com>
2023-03-27 10:34:56 +0530

    connect button

--- component/VideoChat.tsx
+++ component/VideoChat.tsx
@@ -8,27 +8,30 @@ import {
   addDoc,
   setDoc,
   onSnapshot,
-  getDoc,
+  getDocs,
   updateDoc,
+  query,
+  where,
+  limit,
+  getDoc,
+  DocumentReference,
+  DocumentData,
 } from "firebase/firestore";
 // import { collection, doc } from "firebase/firestore";
 
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
 const VideoChat = () => {
   const localVideoRef = useRef<HTMLVideoElement | null>(null);
   const remoteVideoRef = useRef<HTMLVideoElement | null>(null);
   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
 
-  const servers = {
-    iceServers: [
-      {
-        urls: [
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-        ],
-      },
-    ],
-    iceCandidatePoolSize: 10,
-  };
   // Global State
   const [pc, setPc] = useState<RTCPeerConnection | null>(null);
   if (remoteVideoRef.current) remoteVideoRef.current.srcObject = remoteStream;
@@ -50,35 +53,37 @@ const VideoChat = () => {
     }
     init();
   }, []);
+  useEffect(() => {
+    if (pc) {
+      pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteStream?.addTrack(track);
+        });
+      };
+      const callDoc = doc(collection(db, "calls"));
+      const offerCandidates = collection(callDoc, "offerCandidates");
+      // Get candidates for caller, save to db
+      pc!.onicecandidate = (event) => {
+        event.candidate && addDoc(offerCandidates, event.candidate?.toJSON());
+      };
+    }
+  }, [pc]);
   //createRoom
-  const [roomLink, setRoomLink] = useState("");
-  const [joinLink, setJoinLink] = useState("");
-
   async function createRoom() {
-    // Reference Firestore collections for signaling
-    const callDoc = doc(collection(db, "calls"));
-
-    const offerCandidates = collection(callDoc, "offerCandidates");
-    const answerCandidates = collection(callDoc, "answerCandidates");
-
-    setRoomLink(callDoc.id);
-
-    // Get candidates for caller, save to db
-    pc!.onicecandidate = (event) => {
-      event.candidate && addDoc(offerCandidates, event.candidate?.toJSON());
-    };
-
+    console.log("creating room");
     // Create offer
     const offerDescription = await pc!.createOffer();
-    console.log(offerDescription);
-    console.log("valid", offerDescription);
-
     await pc!.setLocalDescription(offerDescription);
     const offer = {
       sdp: offerDescription.sdp,
       type: offerDescription.type,
     };
-    setDoc(callDoc, { offer });
+    // Reference Firestore collections for signaling
+    const callDoc = await addDoc(collection(db, "calls"), {
+      offer,
+      answer: null,
+    });
+    const answerCandidates = collection(callDoc, "answerCandidates");
 
     // Listen for remote answer
     onSnapshot(callDoc, (snapshot) => {
@@ -94,15 +99,13 @@ const VideoChat = () => {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           const candidate = new RTCIceCandidate(change.doc.data());
-          console.log("ICE CANDIDATES ARE: ", candidate);
-
           pc!.addIceCandidate(candidate);
         }
       });
     });
   }
-  async function joinRoom() {
-    const callDoc = doc(db, "calls", joinLink);
+  async function joinRoom(callDoc: DocumentReference<DocumentData>) {
+    console.log("joining room");
     const offerCandidates = collection(callDoc, "offerCandidates");
     const answerCandidates = collection(callDoc, "answerCandidates");
     pc!.onicecandidate = (event) => {
@@ -111,7 +114,7 @@ const VideoChat = () => {
 
     const callData = (await getDoc(callDoc)).data();
 
-    const offerDescription = callData && callData.offer;
+    const offerDescription = callData!.offer;
     await pc!.setRemoteDescription(new RTCSessionDescription(offerDescription));
     const answerDescription = await pc!.createAnswer();
     await pc!.setLocalDescription(answerDescription);
@@ -125,7 +128,6 @@ const VideoChat = () => {
 
     onSnapshot(offerCandidates, (snapshot) => {
       snapshot.docChanges().forEach((change) => {
-        console.log(change);
         if (change.type === "added") {
           let data = change.doc.data();
           pc!.addIceCandidate(new RTCIceCandidate(data));
@@ -133,22 +135,20 @@ const VideoChat = () => {
       });
     });
   }
-  function start() {
-    pc!.ontrack = (event) => {
-      event.streams[0].getTracks().forEach((track) => {
-        remoteStream?.addTrack(track);
-      });
-    };
-  }
+  const connect = async () => {
+    const collectionRef = collection(db, "calls");
+    const q = query(collectionRef, where("answer", "==", null), limit(1));
+
+    const qSnapshot = await getDocs(q);
+    if (qSnapshot.docs.length > 0) {
+      joinRoom(qSnapshot.docs[0].ref);
+    } else {
+      createRoom();
+    }
+  };
   return (
     <main className={styles.main}>
-      <div>
-        <video
-          className={styles.localVdo}
-          ref={localVideoRef}
-          autoPlay
-          playsInline
-        />
+      <div className={styles.remoteVdoWrapper}>
         <video
           className={styles.remoteVdo}
           ref={remoteVideoRef}
@@ -156,27 +156,91 @@ const VideoChat = () => {
           playsInline
         />
       </div>
-      <button onClick={start}>Start vdo</button>
-      <h2>1. Create Room</h2>
-      <button onClick={createRoom}>Create Call (offer)</button>
-      <h3>Link: {roomLink}</h3>
-
-      <h2>2. Join Room</h2>
-      <p>Answer the call from a different browser window or device</p>
-      <input
-        type="text"
-        value={joinLink}
-        onChange={(e) => setJoinLink(e.currentTarget.value)}
-      />
-      <button id="answerButton" onClick={joinRoom}>
-        Join Room
-      </button>
-
-      <h2>4. Hangup</h2>
-
-      <button id="hangupButton">Hangup</button>
+      <div className={styles.form}>
+        <video
+          className={styles.localVdo}
+          ref={localVideoRef}
+          autoPlay
+          muted
+          playsInline
+        />
+        <section>
+          <Button onClick={connect} variant="contained">
+            Connect
+          </Button>
+        </section>
+      </div>
     </main>
   );
 };
 
 export default VideoChat;
+
+// async function createRoom() {
+//   // Reference Firestore collections for signaling
+//   const callDoc = doc(collection(db, "calls"));
+
+//   const answerCandidates = collection(callDoc, "answerCandidates");
+
+//   setRoomLink(callDoc.id);
+
+//   // Create offer
+//   const offerDescription = await pc!.createOffer();
+//   await pc!.setLocalDescription(offerDescription);
+//   const offer = {
+//     sdp: offerDescription.sdp,
+//     type: offerDescription.type,
+//   };
+
+//   setDoc(callDoc, { offer });
+
+//   // Listen for remote answer
+//   onSnapshot(callDoc, (snapshot) => {
+//     const data = snapshot.data();
+//     if (!pc!.currentRemoteDescription && data?.answer) {
+//       const answerDescription = new RTCSessionDescription(data.answer);
+//       pc!.setRemoteDescription(answerDescription);
+//     }
+//   });
+
+//   // When answered, add candidate to peer connection
+//   onSnapshot(answerCandidates, (snapshot) => {
+//     snapshot.docChanges().forEach((change) => {
+//       if (change.type === "added") {
+//         const candidate = new RTCIceCandidate(change.doc.data());
+//         pc!.addIceCandidate(candidate);
+//       }
+//     });
+//   });
+// }
+// async function joinRoom() {
+//   const callDoc = doc(db, "calls", joinLink);
+//   const offerCandidates = collection(callDoc, "offerCandidates");
+//   const answerCandidates = collection(callDoc, "answerCandidates");
+//   pc!.onicecandidate = (event) => {
+//     event.candidate && addDoc(answerCandidates, event.candidate?.toJSON());
+//   };
+
+//   const callData = (await getDoc(callDoc)).data();
+
+//   const offerDescription = callData && callData.offer;
+//   await pc!.setRemoteDescription(new RTCSessionDescription(offerDescription));
+//   const answerDescription = await pc!.createAnswer();
+//   await pc!.setLocalDescription(answerDescription);
+//   const answer = {
+//     type: answerDescription.type,
+//     sdp: answerDescription.sdp,
+//   };
+//   await updateDoc(callDoc, {
+//     answer,
+//   });
+
+//   onSnapshot(offerCandidates, (snapshot) => {
+//     snapshot.docChanges().forEach((change) => {
+//       if (change.type === "added") {
+//         let data = change.doc.data();
+//         pc!.addIceCandidate(new RTCIceCandidate(data));
+//       }
+//     });
+//   });
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import VideoChat from '../component/VideoChat'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import VideoChat from "../component/VideoChat";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,11 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <VideoChat/>
-      </main>
-
-      
+      <VideoChat />
     </div>
-  )
+  );
 }
