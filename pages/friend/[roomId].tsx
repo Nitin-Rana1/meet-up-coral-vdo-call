061c1267d99eeeaeafe0d68b43dd1efbb31f0761
Nitin Rana <nitinranagraphian@gmail.com>
2023-07-14 17:39:47 +0530

    make share url link

--- pages/friend/[roomId].tsx
+++ pages/friend/[roomId].tsx
@@ -53,6 +53,7 @@ import {
   Close,
 } from "@mui/icons-material";
 import { useRouter } from "next/router";
+import Image from "next/image";
 const servers = {
   iceServers: [
     {
@@ -63,15 +64,12 @@ const servers = {
 };
 export default function JoinFriend() {
   const router = useRouter();
-  const { roomId } = router.query;
-  useEffect(() => {
-    if (roomId) joinRoom(roomId[0]);
-  }, []);
-
   const localVideoRef = useRef<HTMLVideoElement | null>(null);
   const remoteVideoRef = useRef<HTMLVideoElement | null>(null);
   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
   const [pc, setPc] = useState<RTCPeerConnection | null>(null);
+  const [createdLink, setCreatedLink] = useState("");
+  const [joiningLink, setJoiningLink] = useState("");
 
   useEffect(() => {
     if (remoteVideoRef.current) {
@@ -107,7 +105,56 @@ export default function JoinFriend() {
       };
     } else console.log("not pc ");
   }, [pc, remoteStream]);
+  //createRoom
+  async function createRoom() {
+    if (pc) console.log("PC creating room");
+    else console.log("creating room");
+    // Create offer
+    const offerDescription = await pc!.createOffer();
+    const offer = {
+      sdp: offerDescription.sdp,
+      type: offerDescription.type,
+    };
+    // Reference Firestore collections for signaling
+    const callDoc = await addDoc(collection(db, "calls"), {
+      offer,
+      answer: null,
+    });
+    setCreatedLink(callDoc.id);
+    //offercandidates
+    const offerCandidates = collection(callDoc, "offerCandidates");
+    const oc = collection(callDoc, "TOP G");
 
+    // Get candidates for caller, save to db
+    pc!.onicecandidate = (event) => {
+      console.log("In under onICEcandidate");
+      if (!event.candidate) console.log("EVENT hi ni hai");
+      else console.log("Event !!!");
+      event.candidate && addDoc(offerCandidates, event.candidate?.toJSON());
+    };
+    await pc!.setLocalDescription(offerDescription);
+
+    const answerCandidates = collection(callDoc, "answerCandidates");
+
+    // Listen for remote answer
+    onSnapshot(callDoc, (snapshot) => {
+      const data = snapshot.data();
+      if (!pc!.currentRemoteDescription && data?.answer) {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc!.setRemoteDescription(answerDescription);
+      }
+    });
+
+    // When answered, add candidate to peer connection
+    onSnapshot(answerCandidates, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          const candidate = new RTCIceCandidate(change.doc.data());
+          pc!.addIceCandidate(candidate);
+        }
+      });
+    });
+  }
   async function joinRoom(id: string) {
     console.log("joining room");
     const callDoc = doc(db, "calls", id);
@@ -176,6 +223,7 @@ export default function JoinFriend() {
       track.enabled = true;
     });
   };
+  const [notJoined, setNotJoined] = useState(true);
   return (
     <Container className={styles.container}>
       <section className={styles.videos}>
@@ -228,6 +276,208 @@ export default function JoinFriend() {
           </div>
         </article>
       </section>
+      {/* <SwipeableTemporaryDrawer
+          createdLink={createdLink}
+          joiningLink={joiningLink}
+          setJoiningLink={setJoiningLink}
+          setCreatedLink={setCreatedLink}
+          createRoom={createRoom}
+          joinRoom={joinRoom}
+          pc={pc}
+        /> */}
+      {notJoined && (
+        <section className={styles.notJoined}>
+          <div>
+            <div className={styles.logoWrapper}>
+              <div className={styles.logo}>
+                <Image src="/logo.png" alt="logo" fill={true} />
+              </div>
+            </div>
+            <h2>You are invited for a video call</h2>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                variant="outlined"
+                sx={{}}
+                onClick={() => {
+                  setNotJoined(false);
+                  joinRoom(router.query.roomId as string);
+                }}
+              >
+                Join Room
+              </Button>
+            </div>
+          </div>
+        </section>
+      )}
     </Container>
+  );
+}
+
+function SwipeableTemporaryDrawer({
+  createdLink,
+  joiningLink,
+  setJoiningLink,
+  setCreatedLink,
+  createRoom,
+  joinRoom,
+  pc,
+}: {
+  createdLink: string;
+  joiningLink: string;
+  setJoiningLink: (x: string) => void;
+  setCreatedLink: (x: string) => void;
+  createRoom: () => void;
+  joinRoom: (x: string) => void;
+  pc: RTCPeerConnection | null;
+}) {
+  const [bottom, setBottom] = useState(true);
+
+  const [invalidJoiningLink, setInvalidJoiningLink] = useState(false);
+  const [tabNo, setTabNo] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabNo(newValue);
+  };
+  useEffect(() => {
+    setBottom(true);
+  }, []);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setBottom(open);
+    };
+
+  const list = () => (
+    <Box
+      sx={{
+        width: "auto",
+        marginBottom: "2vh",
+        padding: "2vw",
+        position: "relative",
+      }}
+      role="presentation"
+      // onClick={toggleDrawer(false)}
+      // onKeyDown={toggleDrawer(false)}
+    >
+      <Tabs
+        value={tabNo}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Join Room" onClick={() => setTabNo(0)} />
+        <Tab label="Create Room" onClick={() => setTabNo(1)} />
+      </Tabs>
+      <Close className={styles.settingIconInMenu} onClick={settingClick} />
+      {tabNo == 0 && (
+        <section
+          style={{
+            marginTop: "5vh",
+            padding: "4vw",
+          }}
+        >
+          <TextField
+            label="Paste Video Call Link"
+            value={joiningLink}
+            fullWidth
+            onChange={(event) => setJoiningLink(event.target.value)}
+            variant="outlined"
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              if (joiningLink == "") {
+                setInvalidJoiningLink(true);
+              } else joinRoom(joiningLink);
+            }}
+            fullWidth
+          >
+            Join Room
+          </Button>
+        </section>
+      )}
+      {tabNo == 1 && (
+        <section
+          style={{
+            marginTop: "5vh",
+            padding: "4vw",
+          }}
+        >
+          <Typography variant="body1" align="center" gutterBottom>
+            Your Room Link : {createdLink == "" ? "None!" : createdLink}
+          </Typography>
+          <Button
+            variant="text"
+            onClick={() =>
+              navigator.share({
+                text: "https://meet-up-coral.vercel.app/friend/" + createdLink,
+                title: "Connect with me on Video chat",
+              })
+            }
+          >
+            <Share style={{ marginRight: "1vw" }} />
+            Share link!
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={createRoom}
+            fullWidth
+            sx={{
+              marginTop: "1.5vw",
+              // backgroundColor: "#1e88e5",
+              // "&:hover": {
+              //   backgroundColor: "#1565c0",
+              // },
+            }}
+          >
+            Create Room
+          </Button>
+          <Dialog
+            open={invalidJoiningLink}
+            onClose={() => setInvalidJoiningLink(false)}
+          >
+            <DialogTitle>Error</DialogTitle>
+            <DialogContent>
+              <Typography>
+                Please enter a valid room link to join the call.
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setInvalidJoiningLink(false)}>OK</Button>
+            </DialogActions>
+          </Dialog>
+        </section>
+      )}
+    </Box>
+  );
+  function settingClick() {
+    setBottom((prevValue) => !prevValue);
+  }
+  return (
+    <div>
+      <div>
+        <Settings className={styles.settingIcon} onClick={settingClick} />
+      </div>
+      <Fragment>
+        <SwipeableDrawer
+          anchor={"bottom"}
+          open={bottom}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          {list()}
+        </SwipeableDrawer>
+      </Fragment>
+    </div>
   );
 }
